db214da8506d01dbf5b6b33a8f8ba20ea29687e3
luocaca <471474615@qq.com>
2017-06-14 22:39:25 +0800

    家里 commit

--- app/src/main/java/com/hldj/hmyg/StoreActivity.java
+++ app/src/main/java/com/hldj/hmyg/StoreActivity.java
@@ -1591,14 +1591,36 @@ public class StoreActivity extends NeedSwipeBackActivity implements
             sp5.setText(text);
 //            sp5.setUrl("http://download.csdn.net/detail/liufatao/9844983?web=web");
 //            sp5.setImageUrl(TextUtils.isEmpty(img) ? GetServerUrl.ICON_PAHT : img);
+            //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此
             sp5.setSiteUrl(url);
-            sp5.setImagePath(TextUtils.isEmpty(img) ? GetServerUrl.ICON_PAHT : img);
+            sp5.setUrl(url);
+            sp5.setImageUrl(TextUtils.isEmpty(img) ? GetServerUrl.ICON_PAHT : img);
 //            sp5.setImagePath("http://f11.baidu.com/it/u=1805479168,3437334559&fm=76");
             Platform qzone = ShareSDK.getPlatform(QQ.NAME);
             qzone.setPlatformActionListener(this); // 设置分享事件回调
             // 执行图文分享
             qzone.share(sp5);
         }
+        /**
+         * // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
+         oks.setTitle("标题");
+         // titleUrl是标题的网络链接，QQ和QQ空间等使用
+         oks.setTitleUrl("http://blog.csdn.net/u013451048");
+         // text是分享文本，所有平台都需要这个字段
+         oks.setText("功能测试，请自动忽略");
+         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+         //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+         oks.setImageUrl("http://avatar.csdn.net/C/3/D/1_u013451048.jpg");
+         // url仅在微信（包括好友和朋友圈）中使用
+         //oks.setUrl("http://sharesdk.cn");
+         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+         oks.setComment("功能测试：qq空间评论");
+         // site是分享此内容的网站名称，仅在QQ空间使用
+         oks.setSite("功能测试：app name");
+         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+         // 启动分享GUI
+         oks.show(this);
+         */
 
     }
 
